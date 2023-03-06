0b7fbe43e738295d64cca0313352266cb703d410
Alex Clarke <informationcake@gmail.com>
2023-03-06 15:50:52 +0000

    Update download-quick.sh

--- scripts/download-quick.sh
+++ scripts/download-quick.sh
@@ -1,9 +1,13 @@
 # this script will search for and grab the fits file list from the main script, and download them in parallel
 
 # get list of files
-grep wget J013151.00+303432.0.sh | cut -c 17- > files.txt
+grep "wget -q" J013151.00+303432.0.sh | cut -c 17- > files.txt
 
 # download files in parallel
-cat files.txt | xargs -P 30 -I{} wget {}
+cat files.txt | xargs -P 30 -I{} wget -nc -q {}
+
+# decompress files in parallel
+grep wget J013151.00+303432.0.sh | cut -c 84- > bz2-files.txt
+cat bz2-files.txt | xargs -P 30 -I{} bzip2 -d -k {}
 
 # Then run ./J013151.00+303432.0.sh to make the mosaic
