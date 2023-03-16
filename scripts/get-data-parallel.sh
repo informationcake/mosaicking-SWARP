5d4d35aceeb507490743a1871ac0b1e95fa26a0f
Alex Clarke <informationcake@gmail.com>
2023-03-16 13:37:06 +0000

    Update get-data-parallel.sh

--- scripts/get-data-parallel.sh
+++ scripts/get-data-parallel.sh
@@ -5,9 +5,11 @@
 grep "https" $1 | cut -c 17- > files.txt
 
 # download files in parallel
+echo "downloading files..."
 cat files.txt | xargs -P 30 -I{} wget -nc -q {}
 
 # decompress files in parallel
+echo "decompressing files..."
 grep https $1 | cut -c 84- > bz2-files.txt
 cat bz2-files.txt | xargs -P 30 -I{} bzip2 -d -k {}
 
