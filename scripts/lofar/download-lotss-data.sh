793704f78b11e3dceb142d95fb8ffc0f2cc39bcb
Alex Clarke <informationcake@gmail.com>
2023-03-16 14:44:47 +0000

    Create download-lotss-data.sh

---
+++ scripts/lofar/download-lotss-data.sh
@@ -0,0 +1,5 @@
+# download some LOFAR survey fields
+# https://lofar-surveys.org/dr2_release.html
+
+wget -nq https://lofar-surveys.org/public/DR2/mosaics/P004+38/mosaic-blanked.fits
+wget -nq https://lofar-surveys.org/public/DR2/mosaics/P004+41/mosaic-blanked.fits
