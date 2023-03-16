03a47b4391d21dd9372e9d8caaff75884972b5a3
Alex Clarke <informationcake@gmail.com>
2023-03-16 11:56:05 +0000

    Update download-data-parallel.sh

--- scripts/large/download-data-parallel.sh
+++ scripts/large/download-data-parallel.sh
@@ -1,13 +1,14 @@
-# this script will search for and grab the fits file list from the main script, and download them in parallel
+# this script will search for and grab the fits file list from the main script, and download them in parallel.
+# it requires the J*.sh file from the SDSS mosaic generation server to grab the list of requires fields.
 
 # get list of files
-grep "wget -q" J013151.00+303432.0.sh | cut -c 17- > files.txt
+grep "https" $1 | cut -c 17- > files.txt
 
 # download files in parallel
 cat files.txt | xargs -P 30 -I{} wget -nc -q {}
 
 # decompress files in parallel
-grep wget J013151.00+303432.0.sh | cut -c 84- > bz2-files.txt
+grep https $1 | cut -c 84- > bz2-files.txt
 cat bz2-files.txt | xargs -P 30 -I{} bzip2 -d -k {}
 
 # Then run ./J013151.00+303432.0.sh to make the mosaic
