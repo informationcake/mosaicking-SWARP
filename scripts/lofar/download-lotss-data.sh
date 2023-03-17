2b6d6bfdde88bfc936f8b1cbf1eab6294e1175f0
Alex Clarke <informationcake@gmail.com>
2023-03-17 11:36:57 +0000

    Update download-lotss-data.sh

--- scripts/lofar/download-lotss-data.sh
+++ scripts/lofar/download-lotss-data.sh
@@ -1,5 +1,12 @@
 # download some LOFAR survey fields
 # https://lofar-surveys.org/dr2_release.html
 
-wget -nq https://lofar-surveys.org/public/DR2/mosaics/P004+38/mosaic-blanked.fits
-wget -nq https://lofar-surveys.org/public/DR2/mosaics/P004+41/mosaic-blanked.fits
+wget -nc -O P003+36.fits https://lofar-surveys.org/public/DR2/mosaics/P003+36/mosaic-blanked.fits
+
+wget -nc -O P004+33.fits https://lofar-surveys.org/public/DR2/mosaics/P004+33/mosaic-blanked.fits
+wget -nc -O P004+38.fits https://lofar-surveys.org/public/DR2/mosaics/P004+38/mosaic-blanked.fits
+wget -nc -O P004+41.fits https://lofar-surveys.org/public/DR2/mosaics/P004+41/mosaic-blanked.fits
+
+wget -nc -O P005+28.fits https://lofar-surveys.org/public/DR2/mosaics/P005+28/mosaic-blanked.fits
+wget -nc -O P006+31.fits https://lofar-surveys.org/public/DR2/mosaics/P006+31/mosaic-blanked.fits
+wget -nc -O P006+36.fits https://lofar-surveys.org/public/DR2/mosaics/P006+36/mosaic-blanked.fits
