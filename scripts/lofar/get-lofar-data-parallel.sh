2be61e85069ccfde364dcb963dc135567ec8f009
Alex Clarke <informationcake@gmail.com>
2023-03-17 12:57:37 +0000

    Update get-lofar-data-parallel.sh

--- scripts/lofar/get-lofar-data-parallel.sh
+++ scripts/lofar/get-lofar-data-parallel.sh
@@ -1,4 +1,4 @@
-# download some LOFAR survey fields
+# download some LOFAR survey fields and make a mosaic
 # assuming you have retrieved a file list from the LOFAR surveys website and called it lofar-files.csv
 
 # get list of file locations
@@ -11,5 +11,11 @@ cat lofar-files.txt | xargs -P 30 -I{} wget -nc -q {}
 # rename files to have .fits
 ls P* | while read line; do mv $line $line$".fits"; done
 
-# Now run swarp from the terminal as:
-# swarp `ls P*.fits`
+# Now run swarp (ensure you are inside the container)
+swarp `ls P*.fits`
+
+# ---
+
+# Whilst SDSS data has the issue with wget not working from inside the container, LOFAR data does not have this issue. 
+
+# Therefore you can run this whole script from inside the container, so uncomment the swarp line for full automation.
